70415741f9823e4052004edf11aecd7114e08c42
Scott Krulcik <skrulcik@gmail.com>
2015-02-19 21:49:22 -0500

    Initial Commit

---
+++ AudioTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioTest
+//
+//  Created by Scott Krulcik on 2/19/15.
+//  Copyright (c) 2015 Scott Krulcik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
